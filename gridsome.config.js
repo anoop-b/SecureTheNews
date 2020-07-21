657b6f0229589268d0f2a9a7571f26d1d2a5a835
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-07-21 12:02:08 +0530

    rename site title

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Secure The News',
   templates: {
     BlogPost: [
       {
